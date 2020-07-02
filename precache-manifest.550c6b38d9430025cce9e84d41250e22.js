09beeb4510655b92366c8f557e76e180c02f44e6
Aleksandar <aleksandar.naskovich@gmail.com>
2020-07-02 09:49:47 +0200

    Updates

---
+++ precache-manifest.550c6b38d9430025cce9e84d41250e22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4cb68bd86d59a05d35686cf336adaa",
+    "url": "/restaurant-website/index.html"
+  },
+  {
+    "revision": "b9b7868b85ee70cc3d7f",
+    "url": "/restaurant-website/static/css/main.314f38f2.chunk.css"
+  },
+  {
+    "revision": "5a6ef3931c3137ac883d",
+    "url": "/restaurant-website/static/js/2.c83bed82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/restaurant-website/static/js/2.c83bed82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b7868b85ee70cc3d7f",
+    "url": "/restaurant-website/static/js/main.8bbe8c22.chunk.js"
+  },
+  {
+    "revision": "02126e8c8cff462af891",
+    "url": "/restaurant-website/static/js/runtime-main.bb74030d.js"
+  },
+  {
+    "revision": "a2373d8bf821fc3341212d14083e961e",
+    "url": "/restaurant-website/static/media/banner-home.a2373d8b.jpg"
+  },
+  {
+    "revision": "a92c5f547cbaf15cf15fe4cd333e0264",
+    "url": "/restaurant-website/static/media/menu-bg.a92c5f54.png"
+  }
+]);
